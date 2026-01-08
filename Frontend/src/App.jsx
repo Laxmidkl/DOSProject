f88b4077fb1b53fbc624988ccd1b8f250e38ab26
Laxmidkl <dhakalaxmi74@gmail.com>
2026-01-08 15:09:21 +0545

    Add blog page banner with framer-motion animations

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/LandingPage/Home'
 import Layout from './components/layout/Layout'
 import About from './pages/About/About'
+import Blog from './pages/Blog/Blog'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
     <Route path='' element ={<Home/>}/>
     <Route path='/about' element={<About/>}/>
+    <Route path='/blog' element={<Blog/>}/>
 
     </Route>
 
--- Frontend/src/pages/Blog/Blog.jsx
+++ Frontend/src/pages/Blog/Blog.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import { FaArrowLeftLong } from 'react-icons/fa6';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+      <div className="min-h-screen">
+        {/* Banner */}
+        <section className="relative">
+          <img
+            src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_1280.jpg"
+            alt="Blogs Banner"
+            className="w-full h-130 object-cover"
+          />
+          <div className="absolute inset-0 bg-black/60"></div>
+
+          <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4">
+            <motion.h2
+              className="text-5xl font-bold h-16 bg-linear-to-r from-sky-300 via-white to-red-300 text-transparent bg-clip-text"
+              initial={{ opacity: 0, y: 40 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+            >
+              Our Blogs
+            </motion.h2>
+
+            <motion.p
+              className="text-gray-200 mt-4 max-w-2xl"
+              initial={{ opacity: 0, y: 40 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.7, delay: 0.2 }}
+            >
+              Explore blogs that simplify IT concepts, language learning, and
+              career guidance to help you grow every day.
+            </motion.p>
+          </div>
+
+       <div className="font-medium absolute left-9 bottom-3 flex gap-1 items-center text-white  cursor-pointer">
+        <div className="hover:text-red-500"><FaArrowLeftLong /></div>
+          <button
+          className=" hover:text-red-500 cursor-pointer"
+          onClick={() =>{
+            navigate("/")
+          }}
+          > Home 
+          </button>  /
+          <span className=" underline"> Blog </span>
+
+        </div>
+
+        </section>
+
+
+    </div>
   )
 }
 
